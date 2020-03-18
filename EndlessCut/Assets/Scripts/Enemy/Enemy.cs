1ef0b0a0f1d61536ce6593c91c13b61aebb57a4a
Team-on <timonsol13@gmail.com>
2020-03-18 16:18:07 +0200

    Done rigid jam 1

---
+++ EndlessCut/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	[SerializeField] float moveSpeed = 2.0f;
+	[SerializeField] bool isFacingRight = true;
+
+	[HideInInspector] [SerializeField] Rigidbody2D rb;
+	[HideInInspector] [SerializeField] Animator anim;
+
+	Vector2[] moveVector;
+	Vector3 prevVelosity;
+
+	private void OnValidate() {
+		if (rb == null)
+			rb = GetComponent<Rigidbody2D>();
+		if (anim == null)
+			anim = GetComponent<Animator>();
+	}
+
+	private void Awake() {
+		moveVector = new Vector2[] {
+			new Vector2(moveSpeed, 0),
+			new Vector2(-moveSpeed, 0),
+		};
+	}
+
+	public void FixedUpdate() {
+		if(moveSpeed != 0.0f)
+			Move(moveVector[isFacingRight ? 0 : 1]);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.transform.tag == "Wall") {
+			Flip();
+			prevVelosity = Vector3.zero;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.transform.tag == "Damage" && moveSpeed != 0.0f) {
+			anim.SetTrigger("Die");
+			moveSpeed = 0.0f;
+			rb.velocity = Vector3.zero;
+			LeanTween.delayedCall(1.0f, () => { Destroy(gameObject); });
+		}
+	}
+
+	private void Move(Vector2 direction) {
+		Vector3 targetVelocity = new Vector2(direction.x, rb.velocity.y);
+		rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref prevVelosity, .05f);
+	}
+
+	private void Flip() {
+		isFacingRight = !isFacingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
--- EndlessCut/Assets/Scripts/Player/Player.cs
+++ EndlessCut/Assets/Scripts/Player/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -13,9 +14,18 @@ public class Player : MonoBehaviour {
 	[SerializeField] SortingLayer layer;
 	[SerializeField] float moveSpeed = 1.0f;
 	[SerializeField] float jumpForce = 1.0f;
+	[SerializeField] GameObject canvas = null;
+	[SerializeField] Slider hpSlider = null;
+	[SerializeField] GameObject cutPrefab = null;
+	[SerializeField] Transform cutPos = null;
+	float hp = 1.0f;
+
+	[SerializeField] float attackDelay = 0.5f;
+	[SerializeField] float currAttackDelay = 0.5f;
 
 	[HideInInspector] [SerializeField] Rigidbody2D rb;
 	[HideInInspector] [SerializeField] SpriteRenderer sp;
+	[HideInInspector] [SerializeField] Animator anim;
 
 	Vector2 m_Look;
 	Vector2 m_Move;
@@ -32,6 +42,8 @@ public class Player : MonoBehaviour {
 			rb = GetComponent<Rigidbody2D>();
 		if(sp == null)
 			sp = GetComponent<SpriteRenderer>();
+		if (anim == null)
+			anim = GetComponent<Animator>();
 	}
 
 	void Awake() {
@@ -41,6 +53,29 @@ public class Player : MonoBehaviour {
 	private void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.transform.tag == "Ground" && collision.transform.position.y < transform.position.y) {
 			isGrounded = true;
+			anim.SetBool("IsGrounded", true);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.transform.tag == "Damage") {
+			TakeHit();
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision) {
+		if (collision.transform.tag == "Ground" && collision.transform.position.y < transform.position.y) {
+			anim.SetBool("IsGrounded", false);
+		}
+	}
+
+	private void Update() {
+		currAttackDelay -= Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.Space) && currAttackDelay < 0) {
+			anim.SetTrigger("Attack");
+			Instantiate(cutPrefab, cutPos.position, Quaternion.identity, null);
+			currAttackDelay = attackDelay;
 		}
 	}
 
@@ -49,14 +84,21 @@ public class Player : MonoBehaviour {
 		Move(m_Move);
 	}
 
+	public void TakeHit() {
+		anim.SetTrigger("TakeHit");
+		hp -= 0.1f;
+		hpSlider.value = hp;
+	}
+
 	public void OnMove(InputAction.CallbackContext context) {
 		m_Move = context.ReadValue<Vector2>();
 		if (m_Move.y < 0)
 			m_Move.y = 0;
-		if (m_Move.sqrMagnitude > 0.5f && isUseActiveHider) {
-			isUseActiveHider = false;
-			currHider.UnHidePlayer();
-			sp.sortingLayerName = layerDefault;
+		if (m_Move.sqrMagnitude > 0.5f) {
+			anim.SetBool("IsMoving", true);
+		}
+		else {
+			anim.SetBool("IsMoving", false);
 		}
 	}
 
@@ -89,6 +131,8 @@ public class Player : MonoBehaviour {
 		}
 
 		if (isGrounded && m_Move.y >= 0.5f) {
+			anim.SetTrigger("Jump");
+			anim.SetBool("IsGrounded", false);
 			isGrounded = false;
 			rb.AddForce(new Vector2(0f, jumpForce));
 		}
@@ -104,6 +148,10 @@ public class Player : MonoBehaviour {
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+
+		theScale = canvas.transform.localScale;
+		theScale.x *= -1;
+		canvas.transform.localScale = theScale;
 	}
 
 	public void Hide() {
